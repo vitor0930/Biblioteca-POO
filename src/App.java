1e2e8ad8cbc234ba22736b1053615560d394c6e9
wagnermarques <wagnerdocri@gmail.com>
2026-02-24 11:08:12 -0300

    Primeiro projeto POO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Usuario u1 = new Usuario("Clark", "clark@email.com", 16);
+
+        System.out.println("Olá "+u1.nome);
+        
     }
 }
---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+    public String nome;
+    public String email;
+    public int idade;
+
+    public Usuario(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+}
